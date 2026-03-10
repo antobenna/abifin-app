0f21ae637c4fc805a398b27ae038dfcf1a74ff20
antobenna <a.benadduce2004@gmail.com>
2026-03-10 23:46:15 +0100

    feat(auth): modifica modello User e relativo factory con gestione ruoli

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,9 +27,15 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'role' => 'cliente',
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function customer(): HasOne 
+    {
+        return this->hasOne(Customer::class);
     }
 
     /**

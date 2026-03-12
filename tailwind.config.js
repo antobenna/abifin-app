f3d2dad3164f6439edb499cda4b4ad6f332ab864
antobenna <a.benadduce2004@gmail.com>
2026-03-12 22:36:34 +0100

    feat(app): setup heroui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
+import { heroui } from "@heroui/react";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -8,6 +10,8 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
+        
+        "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
 
     theme: {
@@ -15,8 +19,49 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: '#0052CC',
+            },
         },
     },
 
-    plugins: [forms],
+
+    darkMode: "class",
+
+
+    plugins: [forms, heroui({
+            themes: {
+                light: {
+                    colors: {
+                        primary: {
+                            DEFAULT: "#0052CC",
+                            foreground: "#FFFFFF",
+                        },
+                        secondary: {
+                            DEFAULT: "#9CA3AF", 
+                            foreground: "#FFFFFF",
+                        },
+                        // Stati delle pratiche 
+                        info: {
+                            DEFAULT: "#7DD3FC", // Azzurrino -> Aperta
+                            foreground: "#0369A1",
+                        },
+                        warning: {
+                            DEFAULT: "#F97316", // Arancione -> In Lavorazione
+                            foreground: "#FFFFFF",
+                        },
+                        success: {
+                            DEFAULT: "#22C55E", // Verde -> Chiusa
+                            foreground: "#FFFFFF",
+                        },
+                        // Gestione Errori e Azioni Distruttive 
+                        danger: {
+                            DEFAULT: "#EF4444", 
+                            foreground: "#FFFFFF",
+                        },
+                        background: "#FFFFFF", 
+                    },
+                },
+            },
+        }),],
 };

79f274c5f5e62cf07e8897b7dfd59267aa023ae3
antobenna <a.benadduce2004@gmail.com>
2026-03-12 17:54:02 +0100

    feat(clienti): resi "required" i campi address e phone in UpdateCustomerRequest

--- app/Http/Requests/Api/UpdateCustomerRequest.php
+++ app/Http/Requests/Api/UpdateCustomerRequest.php
@@ -12,9 +12,9 @@ class UpdateCustomerRequest extends FormRequest
     {
         return [
             'company_name' => ['sometimes', 'required', 'string', 'max:255'],
-            'address'      => ['nullable', 'string', 'max:255'],
+            'address'      => ['sometimes', 'required', 'string', 'max:255'],
             'vat_number'   => ['sometimes', 'required', 'string', 'max:11', 'unique:customers,vat_number,' . $this->route('customer')->id],
-            'phone'        => ['nullable', 'string', 'max:20'],
+            'phone'        => ['sometimes', 'required', 'string', 'max:20'],
             'user_id'      => ['sometimes', 'required', 'integer', 'exists:users,id'],
         ];
     }

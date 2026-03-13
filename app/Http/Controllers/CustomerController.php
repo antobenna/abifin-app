33d660592744205a20fe54f53689a548ff4d378b
antobenna <125251491+antobenna@users.noreply.github.com>
2026-03-13 16:08:06 +0100

    feat(clienti): Fatto in modo che vengano visualizzati al massimo 10 clienti per ogni pagina della tabella

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -21,7 +21,7 @@ class CustomerController extends Controller
 
         if ($request->user()->isAdmin()) {
             // Tutti i clienti paginati per l'admin
-            $customers = Customer::with('user')->orderByDesc('created_at')->orderByDesc('id')->paginate(25);
+            $customers = Customer::with('user')->orderByDesc('created_at')->orderByDesc('id')->paginate(10);
         } else {
             // Solo il record cliente dell'utente autenticato
             $customers = Customer::with('user')
